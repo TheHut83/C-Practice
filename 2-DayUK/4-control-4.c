148a8cb4cedbc780c66eeeb92be72c39c2ae5c5f
TheHut83 <the-hut@hotmail.co.uk>
2024-01-24 16:56:44 +0000

    2-DayUK

---
+++ 2-DayUK/4-control-4.c
@@ -0,0 +1,13 @@
+//
+// Created by TinA on 24/01/2024.
+//
+#include "stdio.h"
+
+int main(){
+    int a=0;
+    do{
+        printf("%d : Printing Hello:\n", a);
+        a++;
+    }while (a<=10);
+    return 0;
+}
