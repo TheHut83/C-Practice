01ce69a5143b001bd899c7abe6b261b95ba775d3
TheHut83 <the-hut@hotmail.co.uk>
2024-02-01 23:16:36 +0000

    5-DayUK

---
+++ 5-DayUK/5.5-ourDeepPj.c
@@ -0,0 +1,13 @@
+//
+// Created by TinA on 30/01/2024.
+//
+#include "stdio.h"
+void ourMainfun();
+int main(){
+    ourMainFun();
+    return 0;
+}
+
+void ourMainFun(){
+    printf("This is our main function");
+}
